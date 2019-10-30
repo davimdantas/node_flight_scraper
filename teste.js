e7be163323e089499985fde64baf6ac335f225a6
davimdantas <davimdantas@gmail.com>
2019-10-30 07:30:13 -0300

    Added EsLint

--- teste.js
+++ teste.js
@@ -1,42 +1,44 @@
-const puppeteer = require("puppeteer");
-const select = require("puppeteer-select");
-var request = require("request");
+'use strict';
+
+const puppeteer = require('puppeteer');
+const select = require('puppeteer-select');
+var request = require('request');
 
 
 async function selectFields(page, id_input, data_selecionada) {
-  console.log("data_selecionada :", data_selecionada);
+  console.log('data_selecionada :', data_selecionada);
   await page.focus(id_input);
   const selector = id_input;
   await page.evaluate(
     ({ id_input }) => {
       var input = document.querySelector(id_input);
-      input.value = "";
+      input.value = '';
     },
-    { id_input }
+    { id_input },
   );
   await page.waitForSelector(selector);
   await page.keyboard.type(data_selecionada);
 
   //   await page.keyboard.type(data_selecionada);
-  await page.keyboard.press("Enter");
+  await page.keyboard.press('Enter');
   await page.waitFor(200);
 }
 
 async function getPrice(page) {
   await page.waitFor(200);
-  preco = await page.evaluate(() => {
-    var campo = document.querySelector("div.fare-details");
+  let preco = await page.evaluate(() => {
+    var campo = document.querySelector('div.fare-details');
     return campo.textContent;
   });
-  console.log("preco :", preco);
+  console.log('preco :', preco);
 
   let bot_token = process.env.TELEGRAM_TOKEN;
-  let bot_chatID = "748527644";
-  send_text =
-  "https://api.telegram.org/bot" +
+  let bot_chatID = '748527644';
+  let send_text =
+  'https://api.telegram.org/bot' +
   bot_token +
-  "/sendMessage?chat_id=" +
-  bot_chatID + '&parse_mode=Markdown&text='+ encodeURI(preco);
+  '/sendMessage?chat_id=' +
+  bot_chatID + '&parse_mode=Markdown&text=' + encodeURI(preco);
   console.log('send_text :', send_text);
   let response = request.get(send_text);
   console.log('response :', response);
@@ -48,19 +50,20 @@ async function selectDepartureCity(
   id_campo,
   cidade,
   classe_campo,
-  cidade_completa
+  cidade_completa,
 ) {
-  console.log("cidade_completa ", cidade_completa);
-  console.log("classe_campo ", classe_campo);
+  console.log('cidade_completa ', cidade_completa);
+  console.log('classe_campo ', classe_campo);
   await page.focus(id_campo);
   await page.keyboard.type(cidade);
   await page.focus(classe_campo);
   // await page.evaluate(({classe_campo, cidade_completa }) => {
-  //     [...document.querySelectorAll(classe_campo)].find(element => element.textContent == cidade_completa).click();
+  //     [...document.querySelectorAll(classe_campo)].
+  // find(element => element.textContent == cidade_completa).click();
   //   }, {classe_campo, cidade_completa});
 
   const element = await select(page).getElement(
-    `${classe_campo}:contains(${cidade_completa})`
+    `${classe_campo}:contains(${cidade_completa})`,
   );
   await element.click();
   // var news = await page.evaluate(({classe_campo, cidade_completa,page }) => {
@@ -111,47 +114,47 @@ async function selectDepartureCity(
 // }
 // }
 
-(async () => {
+(async() => {
   console.clear();
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.voegol.com.br/pt");
+  await page.goto('https://www.voegol.com.br/pt');
   //   await main_teste(page);
-  console.log("22pornyo");
-  await page.waitForSelector("#header-chosen-origin");
-  console.log("pornyo");
-  //   await page.focus("#header-chosen-origin");
+  console.log('22pornyo');
+  await page.waitForSelector('#header-chosen-origin');
+  console.log('pornyo');
+  //   await page.focus('#header-chosen-origin');
   await selectDepartureCity(
     page,
-    "#header-chosen-origin",
-    "Rio de",
-    ".active-result",
-    "Rio de Janeiro - Galeão - GIG"
+    '#header-chosen-origin',
+    'Rio de',
+    '.active-result',
+    'Rio de Janeiro - Galeão - GIG',
   );
-  await page.screenshot({ path: "example1 .png" });
+  await page.screenshot({ path: 'example1 .png' });
 
   await selectDepartureCity(
     page,
-    "#header-chosen-destiny",
-    "Natal",
-    ".active-result",
-    "Natal - NAT"
+    '#header-chosen-destiny',
+    'Natal',
+    '.active-result',
+    'Natal - NAT',
   );
 
-  await page.screenshot({ path: "example 2.png" });
+  await page.screenshot({ path: 'example 2.png' });
 
-  await selectFields(page, "#datepickerGo", "25/12/2019");
-  await page.screenshot({ path: "example 3.png" });
+  await selectFields(page, '#datepickerGo', '25/12/2019');
+  await page.screenshot({ path: 'example 3.png' });
 
-  await selectFields(page, "#datepickerBack", "02/02/2020");
-  await page.screenshot({ path: "example 4.png" });
+  await selectFields(page, '#datepickerBack', '02/02/2020');
+  await page.screenshot({ path: 'example 4.png' });
 
-  await selectFields(page, "#number-adults", "1");
-  await selectFields(page, "#number-kids", "1");
+  await selectFields(page, '#number-adults', '1');
+  await selectFields(page, '#number-kids', '1');
   // # await page.screenshot({'path': 'example.png'})
-  console.log("title ", await page.title());
-  await page.click("button#btn-box-buy"),
-    await page.waitForSelector("div.fare-details");
+  console.log('title ', await page.title());
+  await page.click('button#btn-box-buy');
+  await page.waitForSelector('div.fare-details');
   await getPrice(page);
   browser.close();
 })();
